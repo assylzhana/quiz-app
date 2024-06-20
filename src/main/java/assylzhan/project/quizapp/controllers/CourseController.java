fbdb548169b77d6b304ea4877c434f6cf28d3e17
assylzhana <kabibullaasylzhan@gmail.com>
2024-06-21 02:53:38 +0500

    access changed

--- src/main/java/assylzhan/project/quizapp/controllers/CourseController.java
+++ src/main/java/assylzhan/project/quizapp/controllers/CourseController.java
@@ -3,6 +3,8 @@ package assylzhan.project.quizapp.controllers;
 import assylzhan.project.quizapp.models.Course;
 import assylzhan.project.quizapp.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,27 +17,34 @@ public class CourseController {
     private CourseService courseService;
 
     @PreAuthorize("isAuthenticated()")
-    @GetMapping("view")
-    public List<Course> viewCourses(){
-        return courseService.getCourses();
+    @GetMapping("/view")
+    public ResponseEntity<List<Course>> viewCourses() {
+        List<Course> courses = courseService.getCourses();
+        return ResponseEntity.ok(courses);
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
-    private void addCourse(@RequestBody Course course){
+    public ResponseEntity<Void> addCourse(@RequestBody Course course) {
         courseService.addCourse(course);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
-    private void deleteCourse(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteCourse(@PathVariable Long id) {
         Course course = courseService.findCourseById(id);
         if (course != null) {
             courseService.deleteCourse(course);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
         }
     }
+
     @GetMapping("/search")
-    public List<Course> searchCourses(@RequestParam("searchTerm") String searchTerm) {
-        return courseService.findCourseByName(searchTerm);
+    public ResponseEntity<List<Course>> searchCourses(@RequestParam("searchTerm") String searchTerm) {
+        List<Course> courses = courseService.findCourseByName(searchTerm);
+        return ResponseEntity.ok(courses);
     }
 }
